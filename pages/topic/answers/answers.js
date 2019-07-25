21842714e6301be09492e479d57753276e666b11
734_100007507115_chellel <734_100007507115_chellel@git.cloud.tencent.com>
2019-07-25 17:16:17 +0800

    日常备份

--- pages/topic/answers/answers.js
+++ pages/topic/answers/answers.js
@@ -9,7 +9,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-contentArray:[]
+    contentArray: [],
+    isLoading: false
   },
 
   onItemClick(e) {
@@ -20,8 +21,9 @@ contentArray:[]
     })
   },
   getDataSource() {
-    wx.showLoading({
-      title: '加载中...',
+    wx.showNavigationBarLoading()
+    this.setData({
+      isLoading:true
     })
     var limit = 5;
 
@@ -30,7 +32,7 @@ contentArray:[]
     var test = `https://www.zhihu.com/api/v4/questions/${qId}/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B*%5D.mark_infos%5B*%5D.url%3Bdata%5B*%5D.author.follower_count%2Cbadge%5B*%5D.topics&offset=&limit=3&sort_by=default&platform=desktop`;
     var paging = this.data.paging;
     if (paging) {
-      if(paging.is_end){
+      if (paging.is_end) {
         wx.showToast({
           title: '到底啦',
         })
@@ -41,7 +43,11 @@ contentArray:[]
     }
     console.log(url)
     api.GET(url).then((res) => {
-      console.log(res)
+      wx.hideNavigationBarLoading();
+      this.setData({
+        isLoading: false
+      }) 
+         console.log(res)
       var dataSource = res.data;
       var currDataSource = this.data.dataSource;
       if (Array.isArray(currDataSource) && currDataSource.length > 0)
@@ -51,13 +57,18 @@ contentArray:[]
         paging: res.paging
       })
       var that = this;
-    var  wxParseTemArrayName="contentArray";
+      var wxParseTemArrayName = "contentArray";
       dataSource.forEach((item, index) => {
-        
+
         if (index < this.data[wxParseTemArrayName].length)
           return
         var content = item["content"];
         util.formatWxParse(WxParse, dataSource, content, index, that);
+      })
+    }).catch(e=>{
+      wx.hideNavigationBarLoading();
+      this.setData({
+        isLoading: false
       })
     })
 
@@ -112,6 +123,8 @@ contentArray:[]
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
+    if (this.data.paging.is_end)
+      return
     this.getDataSource();
   },
 
